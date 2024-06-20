43506ce43173d5d970bd7766e569c205042021bf
İlker Toklu <ilkertoklu2000@gmail.com>
2024-06-20 15:43:48 +0300

    Refactor: User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
   def edit; end
 
   def update
-    if @user.update(user_params.merge(address: address_params))
-      redirect_to @user
+    if @user.update(user_update_params)
+      redirect_to @user, notice: 'User was successfully updated.'
     else
       render :edit
     end
@@ -22,19 +22,35 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :username, :email, :phone)
+    params.require(:user).permit(:name, :username, :email, :phone, :website)
   end
 
   def address_params
-    {
-      street: params[:user][:street],
-      suite: params[:user][:suite],
-      city: params[:user][:city],
-      zipcode: params[:user][:zipcode],
-      geo: {
-        lat: params[:user][:geo_lat],
-        lng: params[:user][:geo_lng]
+    params.require(:user).permit(:address_street, :address_suite, :address_city,
+                                 :address_zipcode, address_geo: [:lat, :lng])
+  end
+
+  def company_params
+    params.require(:user).permit(:company_name, :company_catchPhrase, :company_bs)
+  end
+
+  def user_update_params
+    user_params.merge(
+      address: {
+        street: params[:user][:address_street],
+        suite: params[:user][:address_suite],
+        city: params[:user][:address_city],
+        zipcode: params[:user][:address_zipcode],
+        geo: {
+          lat: params[:user][:address_geo_lat],
+          lng: params[:user][:address_geo_lng]
+        }
+      },
+      company: {
+        name: params[:user][:company_name],
+        catchPhrase: params[:user][:company_catchPhrase],
+        bs: params[:user][:company_bs]
       }
-    }
+    )
   end
 end
